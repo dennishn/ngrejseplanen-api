253cf42948f7bb3c84bed8d676a6d43873b37775
Dennis Haulund Nielsen <d.h.n@live.dk>
2017-11-16 16:05:31 +0100

    trying out with ignoring the sw file - still seems like the server invalidates any updates

--- src/app/common/service-worker/sw-update.service.ts
+++ src/app/common/service-worker/sw-update.service.ts
@@ -9,21 +9,12 @@ export class SwUpdateService {
     @Inject(PLATFORM_ID) private platformId: Object
   ) {
     console.log('swUpdateService is browser', isPlatformBrowser(this.platformId));
-    // if(isPlatformBrowser(this.platformId)) {
-      console.log(`its ze browser! eins zwei drei we check for updates ... why does none of the hashes change wtf wtf wtf`);
-      this.swUpdate.available.subscribe(event => {
-        console.log('[App] Update:', event);
-      });
-      this.swUpdate.activated.subscribe(event => {
-        console.log('[App] Activated:', event.current);
-      });
 
-      setTimeout(() => {
-        console.log('calling the thing');
-        this.swUpdate.checkForUpdate().then((e) => {
-          console.warn('checked for update!', e);
-        }).catch((e) => console.error('failed', e));
-      }, 10000);
-    // }
+    this.swUpdate.available.subscribe(event => {
+      console.log('[App] Update:', event);
+    });
+    this.swUpdate.activated.subscribe(event => {
+      console.log('[App] Activated:', event.current);
+    });
   }
 }
